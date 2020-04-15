3fe758b778556b36bf680e2736c7276127627479
TheErrorMaster <mauricio.macias2535@gmail.com>
2020-04-15 14:35:54 -0700

    added firebase to the project

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -1,7 +1,12 @@
+// sources that helped 
+// https://softauthor.com/firestore-querying-filtering-data-for-web
+
 (function(window) {
     'use strict';
     var App = window.App || {};
     var $ = window.jQuery;
+
+    // var ref = database.ref('Users');
 
     function RemoteDataStore(url) {
     if (!url) {
@@ -12,14 +17,22 @@
 
     RemoteDataStore.prototype.add = function (key, val) {
         $.post(this.serverUrl, val, function (serverResponse){
+            // add to firebase
+            database.collection("users").doc(key).set(val);
             console.log(serverResponse);
         });
     };
 
-    RemoteDataStore.prototype.getAll = function (cb) {
+    RemoteDataStore.prototype.getAll = function () {
         $.get(this.serverUrl, function (serverResponse) {
             console.log(serverResponse);
-            cb(serverResponse);
+            // cb(serverResponse);
+            database.collection('users')
+            .get()
+            .then(snap =>
+                snap.forEach(doc => {
+                console.log(doc.data());
+            }))
         });
     };
 
@@ -27,10 +40,17 @@
         $.get(this.serverUrl + '/' + key, function (serverResponse) {
             console.log(serverResponse);
             cb(serverResponse);
+            // get with firebase
+            database.collection("users").doc(key)
+            .get().then(doc => {
+                console.log(doc.data())
+            })
         });
     };
 
     RemoteDataStore.prototype.remove = function (key) {
+        // remove from firebase
+        database.collection('users').doc(key).delete();
         $.ajax(this.serverUrl + '/' + key, {
             type: 'DELETE'
         });
